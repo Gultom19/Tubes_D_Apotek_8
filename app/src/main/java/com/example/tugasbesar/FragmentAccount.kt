d770a14eba1eb42769f148a8d0f8750e2330c8cd
xkendrickz <kendrickchandra128@gmail.com>
2022-09-09 22:09:53 +0700

    revisi

--- app/src/main/java/com/example/tugasbesar/FragmentAccount.kt
+++ app/src/main/java/com/example/tugasbesar/FragmentAccount.kt
@@ -13,33 +13,5 @@ class FragmentAccount: Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return  inflater.inflate(R.layout.fragment_account, container, false)
-
-        val btnPrev = view.findViewById<Button>(R.id.btnLogout)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val btn
-
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val btnPrev = view.findViewById<Button>(R.id.btnLogout)
-
-        btnPrev.setOnClickListener {
-            val builder: AlertDialog.Builder = AlertDialog.Builder(this@FragmentAccount)
-            builder.setMessage("Are you sure want to exit?")
-                .setPositiveButton("YES", object : DialogInterface.OnClickListener{
-                    override fun onClick(dialogInterface: DialogInterface, i: Int){
-                        // Keluar dari aplikasi
-                        finishAndRemoveTask()
-                    }
-                })
-                .show()
-        }
     }
 }
