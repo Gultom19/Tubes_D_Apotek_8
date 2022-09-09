3b8413e2619b88209ef3440d3c6143eb1a264467
Daniel <zikodaniel2@gmail.com>
2022-09-09 21:01:09 +0700

    Tugas Kelompok 8

--- app/src/main/java/com/example/tugasbesar/MainActivity.kt
+++ app/src/main/java/com/example/tugasbesar/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
