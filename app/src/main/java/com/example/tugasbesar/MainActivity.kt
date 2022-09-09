a549f0dc440076082ac1646d0a0c6b7a6b6e35e2
Daniel <zikodaniel2@gmail.com>
2022-09-09 20:42:20 +0700

    Tugas Kelompok 6

--- app/src/main/java/com/example/tugasbesar/MainActivity.kt
+++ app/src/main/java/com/example/tugasbesar/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 
@@ -15,21 +16,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var inputUsername: TextInputLayout
     private lateinit var inputPassword: TextInputLayout
-    private lateinit var mainLayout: ConstraintLayout
+    private lateinit var usernameView: TextInputEditText
+    private lateinit var passwordView: TextInputEditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: TextView
+    lateinit var mBundle: Bundle
+    lateinit var vUsername: String
+    lateinit var vPassword: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setTitle("User Login")
-
+        getSupportActionBar()?.hide()
         inputUsername = findViewById(R.id.inputLayoutUsername)
         inputPassword = findViewById(R.id.inputLayoutPassword)
-        mainLayout = findViewById(R.id.mainLayout)
-        val textBtnSignIn : TextView = findViewById(R.id.textBtnSignUp)
-        val btnLogin: Button = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.textBtnSignUp)
+        btnLogin = findViewById(R.id.btnLogin)
 
-        textBtnSignIn.setOnClickListener {
+        btnSignUp.setOnClickListener {
             val moveRegister = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(moveRegister)
         }
@@ -49,10 +54,81 @@ class MainActivity : AppCompatActivity() {
                 checkLogin = false
             }
 
-            if (username == "bruh" && password == "bruh") checkLogin = true
+            if (username == "a" && password == "b") checkLogin = true
             if (!checkLogin) return@OnClickListener
             val moveHome = Intent(this@MainActivity, HomeActivity::class.java)
             startActivity(moveHome)
         })
+    }
+
+    fun getBundle() {
+        mBundle = intent.getBundleExtra("register")!!
+        vUsername = mBundle.getString("username")!!
+        vPassword = mBundle.getString("password")!!
+    }
+
+    fun setText() {
+        class MainActivity : AppCompatActivity() {
+
+            private lateinit var inputUsername: TextInputLayout
+            private lateinit var inputPassword: TextInputLayout
+            private lateinit var usernameView: TextInputEditText
+            private lateinit var passwordView: TextInputEditText
+            private lateinit var btnLogin: Button
+            private lateinit var btnSignUp: TextView
+            lateinit var mBundle: Bundle
+            lateinit var vUsername: String
+            lateinit var vPassword: String
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+
+                getSupportActionBar()?.hide()
+                inputUsername = findViewById(R.id.inputLayoutUsername)
+                inputPassword = findViewById(R.id.inputLayoutPassword)
+                btnSignUp = findViewById(R.id.textBtnSignUp)
+                btnLogin = findViewById(R.id.btnLogin)
+
+                btnSignUp.setOnClickListener {
+                    val moveRegister = Intent(this, RegisterActivity::class.java)
+                    startActivity(moveRegister)
+                }
+
+                btnLogin.setOnClickListener(View.OnClickListener {
+                    var checkLogin = false
+                    val username: String = inputUsername.getEditText()?.getText().toString()
+                    val password: String = inputPassword.getEditText()?.getText().toString()
+
+                    if (username.isEmpty()) {
+                        inputUsername.setError("Username must be filled with text")
+                        checkLogin = false
+                    }
+
+                    if (password.isEmpty()) {
+                        inputPassword.setError("Password must be filled with text")
+                        checkLogin = false
+                    }
+
+                    if (username == "a" && password == "b") checkLogin = true
+                    if (!checkLogin) return@OnClickListener
+                    val moveHome = Intent(this, HomeActivity::class.java)
+                    startActivity(moveHome)
+                })
+            }
+
+            fun getBundle() {
+                mBundle = intent.getBundleExtra("register")!!
+                vUsername = mBundle.getString("username")!!
+                vPassword = mBundle.getString("password")!!
+            }
+
+            fun setText() {
+                usernameView = findViewById(R.id.etUsername)
+                usernameView.setText(vUsername, TextView.BufferType.EDITABLE)
+                passwordView = findViewById(R.id.etPassword)
+                passwordView.setText(vPassword, TextView.BufferType.EDITABLE)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tugasbesar/RegisterActivity.kt
+++ app/src/main/java/com/example/tugasbesar/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
@@ -17,24 +18,22 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
-        username = findViewById(R.id.inputLayoutUsername)
-        password = findViewById(R.id.inputLayoutPassword)
+        setTitle("Register")
+        username = findViewById(R.id.inputLayoutRegisUsername)
+        password = findViewById(R.id.inputLayoutRegisPassword)
         email = findViewById(R.id.inputLayoutEmail)
         tanggal = findViewById(R.id.inputLayoutTanggalLahir)
         telepon = findViewById(R.id.inputLayoutTelepon)
+        btnRegister =findViewById(R.id.btnRegister)
 
         btnRegister.setOnClickListener{
-            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            val moveMain = Intent(this@RegisterActivity, MainActivity::class.java)
             val mBundle = Bundle()
-            mBundle.putString("username", username.toString())
-            mBundle.putString("password", password.toString())
-            mBundle.putString("email", email.toString())
-            mBundle.putString("tanggal", tanggal.toString())
-            mBundle.putString("telepon", telepon.toString())
-            intent.putExtra("register", mBundle)
+            mBundle.putString("username", username.getEditText()?.getText().toString())
+            mBundle.putString("password", password.getEditText()?.getText().toString())
+            moveMain.putExtra("register", mBundle)
 
-            startActivity(intent)
+            startActivity(moveMain)
         }
     }
 }
