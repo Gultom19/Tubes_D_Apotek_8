bbdd8ac02147fcaf7d7e182294225ee23265ffac
xkendrickz <kendrickchandra128@gmail.com>
2022-09-22 08:12:56 +0700

    Splash screen

--- app/src/main/java/com/example/tugasbesar/room/Note.kt
+++ app/src/main/java/com/example/tugasbesar/room/Note.kt
@@ -1,13 +1,13 @@
 package com.example.tugasbesar.room
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.example.tugasbesar.entity.Obat
-
-@Entity
-data class Note (
-    @PrimaryKey(autoGenerate = true)
-    val obat: String,
-    val jenis: String,
-    val harga: Float
-)
+//import androidx.room.Entity
+//import androidx.room.PrimaryKey
+//import com.example.tugasbesar.entity.Obat
+//
+//@Entity
+//data class Note (
+//    @PrimaryKey(autoGenerate = true)
+//    val obat: String,
+//    val jenis: String,
+//    val harga: Float
+//)
