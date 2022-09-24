96f01de22edb4781afcfb765616d639eb735cd2d
farellrayner <farellrayner@gmail.com>
2022-09-24 08:22:01 +0700

    MyFirebaseMessagingService.kt

--- app/src/main/java/com/example/tugasbesar/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/tugasbesar/MyFirebaseMessagingService.kt
@@ -3,11 +3,13 @@ package com.example.tugasbesar
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.media.RingtoneManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
