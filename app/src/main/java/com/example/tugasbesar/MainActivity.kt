c53a68a58dbd32b41ec0bf56240bff44b182d549
Daniel <zikodaniel2@gmail.com>
2022-09-29 22:33:10 +0700

    Nambahin UGD6_

--- app/src/main/java/com/example/tugasbesar/MainActivity.kt
+++ app/src/main/java/com/example/tugasbesar/MainActivity.kt
@@ -28,18 +28,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.lang.NullPointerException
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.graphics.Color
-import android.os.Build
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.example.tugasbesar.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -140,64 +129,5 @@ class MainActivity : AppCompatActivity() {
         etPassword = findViewById(R.id.etPassword)
         etPassword.setText(vPassword,TextView.BufferType.EDITABLE)
     }
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Notification Title"
-            val descriptionText = "Notification Description"
 
-            val channel1 = NotificationChannel(CHANNEL_ID_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
-                description = descriptionText
-            }
-
-            val channel2 = NotificationChannel(CHANNEL_ID_2, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
-                description = descriptionText
-            }
-
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel1)
-            notificationManager.createNotificationChannel(channel2)
-        }
     }
-
-    private fun sendNotifiaction1() {
-
-        val intent : Intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-        val broadcastIntent : Intent = Intent(this, NotificationReceiver::class.java)
-        broadcastIntent.putExtra("toastMessage", binding?.etMessage?.text.toString())
-        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID_1)
-            .setSmallIcon(R.drawable.ic_baseline_looks_one_24)
-            .setContentTitle(binding?.etTitle?.text.toString())
-            .setContentText(binding?.etMessage?.text.toString())
-            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-            .setColor(Color.BLUE)
-            .setAutoCancel(true)
-            .setOnlyAlertOnce(true)
-            .setContentIntent(pendingIntent)
-            .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        with(NotificationManagerCompat.from(this)) {
-            notify(notificationId1, builder.build())
-        }
-    }
-
-    private fun sendNotifiaction2() {
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID_1)
-            .setSmallIcon(R.drawable.ic_baseline_looks_two_24)
-            .setContentTitle(binding?.etTitle?.text.toString())
-            .setContentText(binding?.etMessage?.text.toString())
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-
-        with(NotificationManagerCompat.from(this)) {
-            notify(notificationId2, builder.build())
-        }
-    }
-}
